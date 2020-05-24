6a531d583fae94ec18dbcbcdb20455ea44b6979b
Adam Lloyd <alloyddesign@gmail.com>
2020-05-24 12:57:26 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <div className="App bg-gray-200">
+      <header className="App-header">
+        <Main />
+      </header>
+    </div>
+  );
+}
+
+export default App;
